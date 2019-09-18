5f8272b8b52a1f2b42efc4f178b9fa18b8b7de91
sachchida <sachchida.singh@sap.com>
2019-09-18 10:01:23 +0530

    updating edit mocks

--- client/src/app/components/edit-mock/edit-mock.component.ts
+++ client/src/app/components/edit-mock/edit-mock.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { Credentials } from '../../models/credentials';
+import {EditMockService} from '../../services/edit-mock.service';
+import { AuthServiceService } from '../../services/auth-service.service';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -10,9 +12,19 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class EditMockComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private cookieService: CookieService, private editMockService: EditMockService,private authServices:AuthServiceService) { }
+  teamName:string;
+  uniqueName:string;
   ngOnInit() {
+    this.teamName = this.cookieService.get('teamName');
+    this.uniqueName=this.cookieService.get('uniqueName');
+  }
+
+  tabchanged(event: Event) {
+    if (event['index'] == 0) {
+
+    } else if (event['index'] == 1) {
+    } 
   }
 
 }
--- controllers/editMockController.js
+++ controllers/editMockController.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
+var serviceModel = require("../models/service");
+var apiEndPoint = require("../models/api_endpoints");
+var soapEndPoint = require("../models/soap_endpoints");
 
 router.post("/update-api", function(req, res, next) {});
 router.post("/update-soap", function(req, res, next) {});
+router.get("/get-rest-endpoints", function(req, res, next) {});
+router.get("/get-soap-endpoints", function(req, res, next) {});
 
 module.exports = router;
